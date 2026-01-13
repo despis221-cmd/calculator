422d5b3c4adf4b887b0022c51ffd12c928a0426e
despis221-cmd <despis221@gmail.com>
2026-01-13 14:52:02 +0900

    결과값 출력하기

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -15,5 +15,33 @@ public class App {
         String arith = scanner.next(); // 입력받은 string arith에 담기
         char ops = arith.charAt(0); // arith의 첫 글자를 문자로 변환하여 ops에 담기
 
+        int result = 0;
+        switch(ops) {
+            case '+':
+                result = posint1 + posint2;
+                break;
+            case '-':
+                result = posint1 - posint2;
+                break;
+            case '*':
+            case 'x':
+            case 'X':
+                result = posint1 * posint2;
+                break;
+            case '/':
+            case '÷':
+                if(posint2 == 0) {
+                    System.out.println("나눗셈 연산에서 분모(두 번째 정수)에 0이 입력될 수 없습니다.");
+                    // break; 사용 시 결과: 0이 출력되어 값을 계산한 것처럼 보임
+                }
+                else {
+                    result = posint1 / posint2;
+                    break;
+                }
+            default:
+                System.out.println("올바른 연산자을 입력하세요. (Hint: + - * /)");
+        }
+
+        System.out.println("결과: " + result);
     }
 }
