7e05fecb167b08d01cdfc9e0559e8b050c664d92
despis221-cmd <despis221@gmail.com>
2026-01-13 20:18:54 +0900

    enum

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -10,30 +10,31 @@ public class Calculator {
         int result = 0;
         boolean error = false; // 상태 확인
 
-        switch (ops) {
-            case '+':
-                result = posint1 + posint2;
-                break;
-            case '-':
-                result = posint1 - posint2;
-                break;
-            case '*':
-            case 'x':
-            case 'X':
-                result = posint1 * posint2;
-                break;
-            case '/':
-            case '÷':
-                if (posint2 == 0) {
-                    System.out.println("나눗셈 연산에서 분모(두 번째 정수)에 0이 입력될 수 없습니다.");
-                    error = true; // 에러 발생
-                } else {
-                    result = posint1 / posint2;
-                }
-                break;
-            default:
-                System.out.println("올바른 연산자을 입력하세요. (Hint: + - * /)");
-                error = true; // 에러 발생
+        OperatorType operatorType = OperatorType.calssify(ops); // enum
+
+        if(operatorType == null) {
+            System.out.println("올바른 연산자을 입력하세요. (Hint: + - * /)");
+            error = true; // 에러 발생
+        } else {
+            switch (operatorType) {
+                case PLUS:
+                    result = posint1 + posint2;
+                    break;
+                case MINUS:
+                    result = posint1 - posint2;
+                    break;
+                case MULTIPLY:
+                    result = posint1 * posint2;
+                    break;
+                case DIVIDE:
+                    if (posint2 == 0) {
+                        System.out.println("나눗셈 연산에서 분모(두 번째 정수)에 0이 입력될 수 없습니다.");
+                        error = true; // 에러 발생
+                    } else {
+                        result = posint1 / posint2;
+                    }
+                    break;
+            }
         }
         if (!error) { // 에러가 발생하지 않았을 때만 출력, 저장
             System.out.println("결과: " + result);
---
+++ src/main/java/OperatorType.java
@@ -0,0 +1,11 @@
+public enum OperatorType { PLUS, MINUS, MULTIPLY, DIVIDE; // 연산자 묶기
+
+    public static OperatorType calssify(char ops) {
+        if (ops == '+') return PLUS;
+        if (ops == '-') return MINUS;
+        if (ops == '*' || ops == 'x' || ops == 'X') return MULTIPLY;
+        if (ops == '/' || ops == '÷') return DIVIDE;
+
+        return null; // 해당되지 않는 연산자
+    }
+}
