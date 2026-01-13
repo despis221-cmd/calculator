f408a8b90a4f14cec0b9ed795232f9b4dc9b3f55
despis221-cmd <despis221@gmail.com>
2026-01-13 16:08:06 +0900

    Calculator 클래스 생성

---
+++ src/main/java/Calculator.java
@@ -0,0 +1,43 @@
+import java.util.ArrayList;
+
+public class Calculator {
+
+    ArrayList<Integer> resultList = new ArrayList<>(); // 결과 저장할 리스트
+
+    public int calculate(int posint1, int posint2, char ops) {
+
+        int result = 0;
+        boolean error = false; // 상태 확인
+
+        switch (ops) {
+            case '+':
+                result = posint1 + posint2;
+                break;
+            case '-':
+                result = posint1 - posint2;
+                break;
+            case '*':
+            case 'x':
+            case 'X':
+                result = posint1 * posint2;
+                break;
+            case '/':
+            case '÷':
+                if (posint2 == 0) {
+                    System.out.println("나눗셈 연산에서 분모(두 번째 정수)에 0이 입력될 수 없습니다.");
+                    error = true; // 에러 발생
+                } else {
+                    result = posint1 / posint2;
+                }
+                break;
+            default:
+                System.out.println("올바른 연산자을 입력하세요. (Hint: + - * /)");
+                error = true; // 에러 발생
+        }
+        if (!error) { // 에러가 발생하지 않았을 때만 출력, 저장
+            System.out.println("결과: " + result);
+            resultList.add(result);
+        }
+        return result;
+    }
+}
