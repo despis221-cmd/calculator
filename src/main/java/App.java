b0b907803e3efa82ea1cc8925d18f94f9aaed9ff
despis221-cmd <despis221@gmail.com>
2026-01-13 13:01:20 +0900

    사칙연산 기호를 입력받기

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -11,5 +11,9 @@ public class App {
         System.out.println("두 번째 숫자를 입력하세요: ");
         int posint2 = scanner.nextInt(); // 입력받은 integer posint2에 담기
 
+        System.out.println("사칙연산 기호를 입력하세요: ");
+        String arith = scanner.next(); // 입력받은 string arith에 담기
+        char ops = arith.charAt(0); // arith의 첫 글자를 문자로 변환하여 ops에 담기
+
     }
 }
